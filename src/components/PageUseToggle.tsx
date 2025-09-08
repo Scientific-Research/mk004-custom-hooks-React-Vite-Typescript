cb7a9b092aff5807051a7e27b3805b10241e2242
Maximilian <scientificresearch309@gmail.com>
2025-09-08 20:28:09 +0200

    definingb the state variable showAds

--- src/components/PageUseToggle.tsx
+++ src/components/PageUseToggle.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+
 interface IProp {
   PageUseToggle: string;
 }
 
 export const PageUseToggle = ({ PageUseToggle }: IProp) => {
+  const [showAds, setShowAds] = useState(false);
+
   return (
     <>
-      <p>Welcome to this site - PageUseToggle - {PageUseToggle}</p>
+      <p className="pageToggle">
+        Welcome to this site - PageUseToggle - {PageUseToggle}
+      </p>
+      <hr />
+      <button className="btnAds">Advertisements</button>
+      {showAds
+        ? 'Yes, show me advertisements'
+        : 'No, do not show me advertisements'}
     </>
   );
 };
